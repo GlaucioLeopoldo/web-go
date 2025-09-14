92d2c030158830352f9075a099f250ba5b5f686d
GlaucioSilva <glaucioleopoldodev@gmail.com>
2025-09-14 18:59:38 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module web
+
+go 1.24.7
+
+require github.com/go-chi/chi/v5 v5.2.3 // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"web/view"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	tmpl, err := view.Parse(filepath)
+	if err != nil {
+		log.Printf("Parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func main() {
+	rtr := chi.NewRouter()
+	rtr.Get("/", homeHandler)
+	rtr.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
+	fmt.Println("Start the server on :8000...")
+	http.ListenAndServe(":8000", rtr)
+}
